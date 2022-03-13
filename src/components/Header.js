071c91a47fb24a12a7abc9686fbadbedb7d42aa6
Kaoz <kaoz415@gmail.com>
2022-03-13 16:40:33 +0100

    gh pages

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,14 +4,10 @@ import Timer from "./Timer";
 
 const Header = (props) => {
 
-    const style = {
-        border: '1px solid red',
-    }
-
     return (
-        <div style={style}>
+        <div>
             <p>You have found: {props.checkFoundCharacters()}</p>
-            <Timer 
+            <Timer
                 gameWon={props.gameWon}></Timer>
         </div>
     )
