ac32466e9ccd64f37cfda529a2a2f0f1c875c15b
Kaoz <kaoz415@gmail.com>
2022-03-13 16:56:34 +0100

    refactor

--- src/App.js
+++ src/App.js
@@ -14,74 +14,45 @@ const App = (props) => {
   const [renderDropdown, setRenderDropdown] = useState(false);
   const [gameWon, setGameWon] = useState(false);
 
-  // const getCoordinates = (e) => {
-  //   let coordsX = e.nativeEvent.offsetX;
-  //   let coordsY = e.nativeEvent.offsetY;
-
-  //   let imgWidth = e.target.offsetWidth;
-  //   let imgHeight = e.target.offsetHeight;
-  //   console.log(coordsX)
-  //   console.log(coordsY)
-  //   console.log(imgWidth)
-  //   console.log(imgHeight)
-
-  //   let xInPercent = coordsX / imgWidth;
-  //   let yInPercent = coordsY / imgHeight;
-
-  //   return {
-  //     coordsX,
-  //     coordsY,
-  //     imgWidth,
-  //     imgHeight,
-  //     xInPercent,
-  //     yInPercent,
-  //   }
-  // }
-
   const handleClick = (e) => {
-      if(coordinates.x <= 521 && coordinates.x >= 495 && coordinates.y <= 309 && coordinates.y >= 262 && e.target.alt === 'wally') {
-        alert('You have found Wally!');
-        setWallyWasClicked(true);
-        setCharacters([...characters,'Wally'])
-        setRenderDropdown(false)
-      } else if(coordinates.x >= 727 && coordinates.x <= 741 && coordinates.y <= 320 && coordinates.y >= 297 && e.target.alt === 'wilma') {
-        alert('You have found Wilma!')
-        setWilmaWasClicked(true);
-        setCharacters([...characters,'Wilma'])
-        setRenderDropdown(false)
-      } else if (coordinates.x >= 0 && coordinates.x <= 12 && coordinates.y >= 244  && coordinates.y <= 282 && e.target.alt === 'wizard') {
-        alert('You have found Wizard!');
-        setWizardWasClicked(true);
-        setCharacters([...characters,'Wizard'])
-        setRenderDropdown(false)
-      } else if (e.target.alt === 'wizard') {
-        alert(`That's not a Wizard!`)
-      } else if (e.target.alt === 'wally') {
-        alert(`That's not a Wally!`)
-      } else if (e.target.alt === 'wilma') {
-        alert(`That's not a Wilma!`)
-      } 
+    if (coordinates.x <= 521 && coordinates.x >= 495 && coordinates.y <= 309 && coordinates.y >= 262 && e.target.alt === 'wally') {
+      alert('You have found Wally!');
+      setWallyWasClicked(true);
+      setCharacters([...characters, 'Wally'])
+      setRenderDropdown(false)
+    } else if (coordinates.x >= 727 && coordinates.x <= 741 && coordinates.y <= 320 && coordinates.y >= 297 && e.target.alt === 'wilma') {
+      alert('You have found Wilma!')
+      setWilmaWasClicked(true);
+      setCharacters([...characters, 'Wilma'])
+      setRenderDropdown(false)
+    } else if (coordinates.x >= 0 && coordinates.x <= 12 && coordinates.y >= 244 && coordinates.y <= 282 && e.target.alt === 'wizard') {
+      alert('You have found Wizard!');
+      setWizardWasClicked(true);
+      setCharacters([...characters, 'Wizard'])
+      setRenderDropdown(false)
+    } else if (e.target.alt === 'wizard') {
+      alert(`That's not a Wizard!`)
+    } else if (e.target.alt === 'wally') {
+      alert(`That's not a Wally!`)
+    } else if (e.target.alt === 'wilma') {
+      alert(`That's not a Wilma!`)
+    }
   }
 
 
   useEffect(() => {
     setRenderDropdown(true);
-    console.log('useeffect triggered')
-  },[wallyWasClicked,wilmaWasClicked,wizardWasClicked])
+  }, [wallyWasClicked, wilmaWasClicked, wizardWasClicked])
 
   const handleDropdown = (e) => {
-    console.log('dropdown start')
     document.getElementById("floatdiv").style.visibility = "visible";
     document.getElementById("floatdiv").style.position = "absolute";
     let x = e.nativeEvent.offsetX;
     let y = e.nativeEvent.offsetY;
 
-    document.getElementById("floatdiv").style.left=x+"px";
-    document.getElementById("floatdiv").style.top=y+"px";
-    console.log(x)
-    console.log(y)
-    setCoordinates({x: x, y: y});
-    console.log('coordinates set', coordinates)
+    document.getElementById("floatdiv").style.left = x + "px";
+    document.getElementById("floatdiv").style.top = y + "px";
+    setCoordinates({ x: x, y: y });
     return {
       x,
       y,
@@ -89,7 +60,7 @@ const App = (props) => {
   }
 
   const checkFoundCharacters = () => {
-    if(wilmaWasClicked && wallyWasClicked && wizardWasClicked) {
+    if (wilmaWasClicked && wallyWasClicked && wizardWasClicked) {
       return `${characters}`
     } else {
       return `${characters}`
@@ -99,36 +70,37 @@ const App = (props) => {
   const Leaderboard = () => {
     let time = document.getElementsByClassName('time')[0].textContent;
     let sum = time.slice(-3).trim();
-    console.log('sum: ', sum);
-    let name = prompt('Please provide your name for the SCORE BOARD')
+    let name = prompt('Please provide your name for the SCORE BOARD');
+    if (name === null || name === '') {
+      name = 'Player';
+    }
     alert(`${name} got ${sum}`);
   }
 
   useEffect(() => {
-    if(wilmaWasClicked && wallyWasClicked && wizardWasClicked) {
+    if (wilmaWasClicked && wallyWasClicked && wizardWasClicked) {
       setGameWon(true);
       alert('You have won');
-      console.log('set game as won');
       Leaderboard();
     }
-  }, [wilmaWasClicked,wallyWasClicked,wizardWasClicked])
+  }, [wilmaWasClicked, wallyWasClicked, wizardWasClicked])
 
   return (
-    <div className="App">  
-    <Header
-      checkFoundCharacters={checkFoundCharacters}
-      characters={characters}
-      gameWon={gameWon}
-    />
+    <div className="App">
+      <Header
+        checkFoundCharacters={checkFoundCharacters}
+        characters={characters}
+        gameWon={gameWon}
+      />
       <img src={game} alt='game' onClick={handleDropdown}></img>
-        {renderDropdown === true ? <Dropdown
-          wilmaWasClicked={wilmaWasClicked}
-          wallyWasClicked={wallyWasClicked}
-          wizardWasClicked={wizardWasClicked}
-          visible={renderDropdown}
-          handleClick={handleClick}
-          onClick={handleDropdown}
-      />: null}
+      {renderDropdown === true ? <Dropdown
+        wilmaWasClicked={wilmaWasClicked}
+        wallyWasClicked={wallyWasClicked}
+        wizardWasClicked={wizardWasClicked}
+        visible={renderDropdown}
+        handleClick={handleClick}
+        onClick={handleDropdown}
+      /> : null}
     </div>
   );
 }
--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -17,20 +17,19 @@ const styleImg = {
 const Dropdown = (props) => {
 
     useEffect(() => {
-        console.log('Dropdown rendered')
     })
 
     return (
         <div id="floatdiv" style={style}>
-           { !props.wallyWasClicked && <div id="wally" value="wally" onClick={props.handleClick}>
+            {!props.wallyWasClicked && <div id="wally" value="wally" onClick={props.handleClick}>
                 <img src={wally} alt='wally' style={styleImg}></img>
                 Wally
             </div>}
-          { !props.wizardWasClicked && <div value="wizard" onClick={props.handleClick}>
+            {!props.wizardWasClicked && <div value="wizard" onClick={props.handleClick}>
                 <img src={wizard} alt="wizard" style={styleImg}></img>
                 Wizard
             </div>}
-          { !props.wilmaWasClicked && <div value="wilma" id="wilma" onClick={props.handleClick}>
+            {!props.wilmaWasClicked && <div value="wilma" id="wilma" onClick={props.handleClick}>
                 <img src={wilma} alt='wilma' style={styleImg}></img>
                 Wilma
             </div>}
